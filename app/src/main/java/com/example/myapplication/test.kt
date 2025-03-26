9542e48fb243494a2ed453d086ec4c1310c260b7
cho-sr <cho020218@gmail.com>
2025-03-26 15:04:57 +0900

    0326

---
+++ app/src/main/java/com/example/myapplication/test.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+fun hofFun(arg:(Int) -> Boolean) : () -> String {
+    val result = if(arg(10)) {
+        "valid"
+    }
+    else{
+        "invalid"
+    }
+    return {"hofFun result :  $result"}
+}
+
+fun main(){
+    val result = hofFun { no -> no > 0 }
+    println(result())
+}
