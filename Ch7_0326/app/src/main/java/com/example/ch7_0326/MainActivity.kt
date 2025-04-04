3bda263ac6340e4e3113354d81a53ace9bc25882
cho-sr <cho020218@gmail.com>
2025-04-04 16:38:11 +0900

    Week3-4

---
+++ Ch7_0326/app/src/main/java/com/example/ch7_0326/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.ch7_0326
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.phoneNumber)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textView = findViewById<TextView>(R.id.phoneNumber)
+        textView.text = "010-1234-5678"
+    }
+
+}
